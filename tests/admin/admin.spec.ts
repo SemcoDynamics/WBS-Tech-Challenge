7ecfd9155a22f22dc20ca9110f3ef86dcf51cc7a
Brandon <bolckers19@gmail.com>
2026-07-11 14:37:31 +0200

    fix/removed imageinput and url from the update a booking test

--- tests/admin/admin.spec.ts
+++ tests/admin/admin.spec.ts
@@ -8,7 +8,6 @@ import { AdminRoomsPage } from '../../pages/adminRoomsPage';
 
 const adminUsername: string = process.env.ADMIN_USERNAME!;
 const adminPassword: string = process.env.ADMIN_PASSWORD!;
-const roomImageUrl: string = 'https://images.pexels.com/photos/19836834/pexels-photo-19836834.jpeg';
 
 let loginPage: LoginPage;
 let loginHelper: LoginHelper;
@@ -28,7 +27,7 @@ test('Create a new booking', async ({ page }) => {
   await loginHelper.quickLogin(adminUsername, adminPassword, '/admin');
   await expect(page).toHaveURL('/admin/rooms', {timeout: 10000});
 
-  await adminRooms.roomName.fill('104');
+  await adminRooms.roomName.fill('105');
   await adminRooms.roomPrice.fill('20');
   await adminRooms.wifiCheckbox.check();
   await adminRooms.refreshmentDiv.click();
@@ -43,14 +42,13 @@ test('Update a booking', async ({ page }) => {
   await loginHelper.quickLogin(adminUsername, adminPassword, '/admin');
   await expect(page).toHaveURL('/admin/rooms', {timeout: 10000});
 
-  await adminRooms.roomListing.filter({ hasText: '104' }).click();
-  await expect(adminRooms.headingRoom).toHaveText('Room: 104');
+  await adminRooms.roomListing.filter({ hasText: '105' }).click();
+  await expect(adminRooms.headingRoom).toHaveText('Room: 105');
   await adminRooms.editButton.click();
   await adminRooms.typeSelect.selectOption('Family');
   await adminRooms.accessibleSelect.selectOption('true');
   await adminRooms.roomPriceInput.fill('30');
   await adminRooms.descriptionInput.fill('This is a family room with accessibility features.');
-  await adminRooms.imageInput.setInputFiles(roomImageUrl);
   await adminRooms.updateButton.click();
 });
 
