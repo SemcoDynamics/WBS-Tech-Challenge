09d14f00a717ef93df6cc0c3d301ba7346be1f83
Brandon <bolckers19@gmail.com>
2026-07-11 14:07:25 +0200

    chore/updated playwright.config.ts to use chromium only

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,16 +46,6 @@ export default defineConfig({
       },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
